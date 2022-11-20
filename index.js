826bde6d59c7241637fc0b6633277f04afa1bb23
karol-brejna-i <karolbrejna@apache.org>
2022-11-20 20:20:43 +0100

    Clean up puppetTools.js; switch to single quotes

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const core = require('@actions/core');
 const tools = require('./tools');
-const puppetTools = require("./puppetTools");
+const {puppetRun} = require('./puppets');
 
 async function run() {
     try {
         const parameters = await tools.getParameters();
-        core.info("Parameters: " + JSON.stringify(parameters));
+        core.info(`Parameters: ${JSON.stringify(parameters)}`);
         const parametersValid = await tools.validateParameters(parameters);
-        core.info("Parameters valid: " + parametersValid);
+        core.info(`Parameters valid: ${parametersValid}`);
 
-        puppetTools.puppetRun(parameters);
+        await puppetRun(parameters);
 
         core.info((new Date()).toTimeString());
         core.setOutput('time', new Date().toTimeString());
-
 
     } catch (error) {
         core.error(error.message);
---
+++ puppets.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const puppeteer = require('puppeteer');
+
+const catchConsole = async function (page) {
+    page.on("pageerror", function (err) {
+        core.info(`Page error: ${err.toString()}`);
+    });
+
+    page.on('error', function (err) {
+        core.info(`Error: ${err.toString()}`);
+    });
+
+    page.on('console', function (message) {
+        core.info(`>${message.text()}`);
+    });
+};
+
+const puppetRun = async function (parameters) {
+    core.info('Puppet run.');
+
+    const beforeScript = parameters['beforeScript'];
+    const urls = [parameters['url']];
+
+    const results = await Promise.all(urls.map(
+        async (url) => {
+            // start the headless browser
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            // capture browser console, if required
+            await catchConsole(page);
+
+            await page.goto(url);
+
+            let result = undefined;
+            if (beforeScript) {
+                core.info('Using beforeScript parameter.');
+
+                const runMyScript = require('./script.js');
+                result = await runMyScript(page, beforeScript);
+                core.info(`Result: ${result}`);
+            }
+
+            await page.screenshot({path: parameters.output, fullPage: false});
+            await browser.close();
+
+            return result;
+        }
+    ));
+};
+
+module.exports = {catchConsole, puppetRun};
