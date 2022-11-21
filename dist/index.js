bccc231bea8ee15f51c87e226d4a1b741dfbd95d
karol-brejna-i <karolbrejna@apache.org>
2022-11-21 12:25:12 +0100

    Manual action run: add some output

--- dist/index.js
+++ dist/index.js
@@ -28158,10 +28158,17 @@ const puppetRun = async function (parameters) {
     const beforeScript = parameters['beforeScript'];
     const urls = [parameters['url']];
 
+    // XXX
+    const launchOptions = {
+        executablePath: 'google-chrome-stable',
+        args: ['--no-sandbox'],
+        headless: true
+    }
+
     const results = await Promise.all(urls.map(
         async (url) => {
             // start the headless browser
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch(launchOptions);
             const page = await browser.newPage();
             // capture browser console, if required
             await catchConsole(page);
