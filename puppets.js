7935636a8784544332fe252edcd21b5a32d73ff7
Karol Brejna <karolbrejna@apache.org>
2022-12-01 16:13:07 +0000

    fixup! Add devcontainer

--- puppets.js
+++ puppets.js
@@ -79,7 +79,10 @@ const puppetRun = async function (parameters) {
 
             let response;
             try {
-                response = await page.goto(url);
+                response = await page.goto(url, {
+                        waitUntil: "networkidle2",
+                    });
+                await page.waitFor(3000);
             } catch (error) {
                 console.log('page.goto() resulted in error: ' + error);
                 core.setFailed(error.message)
